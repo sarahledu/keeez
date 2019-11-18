a6d35312039c38b5f56060202aff149d7e70c9fb
Amine LAHRICHI <amine.lahrichi@dauphine.eu>
2019-11-18 13:49:11 +0100

    Models done

---
+++ models/Investor.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose"); // import mongoose dependencie
+
+const Schema = mongoose.Schema;
+
+const investorSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    phone_number: { type: Number, required: true },
+    matrimonial_status: [
+      "married",
+      "domestic partnership",
+      "single",
+      "divorced",
+      "widowed"
+    ],
+    nbr_child: { type: Number, default: null },
+    total_revenue: { type: Number, default: null },
+    monthly_savings: { type: Number, default: null },
+    annual_revenu_taxes: { type: Number, default: null },
+    objectives: { type: ["Example1", "Example2"], default: null },
+    properties: {
+      type: Schema.Types.ObjectId,
+      ref: "Properties"
+    },
+    areas: {
+      type: ["A une heure de Paris", "A deux heures de train"],
+      default: null
+    },
+    status: { type: Boolean, default: false }
+    // completed_at: {type: Date},
+  },
+  {
+    timestamps: { createdAt: "created_at" }
+  }
+);
+
+const investorModel = mongoose.model("Investor", investorSchema);
+
+module.exports = investorModel;
---
+++ models/Pro.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose"); // import mongoose dependencie
+
+const Schema = mongoose.Schema;
+
+const proSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    phone_number: { type: Number, required: true },
+    form_bought: {
+      type: [{ type: Schema.Types.ObjectId, ref: "Investor" }],
+      default: null
+    }
+  },
+  {
+    timestamps: { createdAt: "created_at" }
+  }
+);
+
+const proModel = mongoose.model("Pro", proSchema);
+
+module.exports = proModel;
