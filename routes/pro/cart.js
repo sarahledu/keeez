2f2f335b7c1b1cc5968e140fa56db437d4fbedff
Amine LAHRICHI <amine.lahrichi@dauphine.eu>
2019-11-19 01:30:25 +0100

    Cart css

--- routes/pro/cart.js
+++ routes/pro/cart.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/pro/cart", (req, res) => {
-  res.render("pro/cart");
+  res.render("pro/cart", { css: ["cart"] });
 });
 
 module.exports = router;
