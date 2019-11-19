5f57bff1a880d592888a5f31602e9fbf404fbb79
Amine LAHRICHI <amine.lahrichi@dauphine.eu>
2019-11-19 12:11:13 +0100

    select debug form investor

--- routes/invest/invest.js
+++ routes/invest/invest.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = new express.Router();
 const isLoggedIn = require("./../../middlewares/isLoggedIn");
 
+// INVESTOR
+const investorModel = require("../../models/Investor");
+
 router.get("/form", isLoggedIn.protectInvestor, (req, res) => {
-  res.render("invest/form");
+  res.render("invest/form", { user: req.session.currentUser });
 });
 router.get("/dashboard", isLoggedIn.protectInvestor, (req, res) => {
   res.render("invest/dashboard");
