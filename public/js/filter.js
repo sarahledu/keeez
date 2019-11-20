44501c72ba4a1eb0fece35dbbc5e3721accfd83f
Amine LAHRICHI <amine.lahrichi@dauphine.eu>
2019-11-20 11:55:45 +0100

    objective filter

--- public/js/filter.js
+++ public/js/filter.js
@@ -29,20 +29,41 @@ const objectives = document.querySelectorAll("[data-obj]");
 
 objectives.forEach(obj => {
   obj.onclick = function(evt) {
-    const objective = obj.getAttribute("data-obj");
+    const checkedElements = [];
+    objectives.forEach(input => {
+      if (input.checked === true)
+        checkedElements.push(input.getAttribute("data-obj"));
+    });
+    console.log(checkedElements);
     axios
-      .post("http://localhost:9090/pro/search", { objectives: objective })
+      .post("http://localhost:9090/pro/search", { objectives: checkedElements })
       .then(myAPIRes => {
-        console.log(myAPIRes.data);
-        console.log(myAPIRes.data, "Here is the client side");
-
-        // const filteredBoxes = myAPIRes.data;
-        // const boxLayout = document.querySelector(".d-grid.boxes");
-        // boxLayout.innerHTML = "";
-        // filteredBoxes.forEach(box => {
-        //     console.log(box)
-        //     boxLayout.innerHTML += `<div class="box" style="background-color:${box.color}">${box.color}</div>`
-        // });
+        const listUsers = myAPIRes.data;
+        const tableContact = document.querySelector("#tbody-contacts");
+        tableContact.innerHTML = "";
+        listUsers.forEach(user => {
+          console.log(user);
+          tableContact.innerHTML += `<tr class="table-row">
+          <td class="table-division">
+            <div>${user.created_at}</div>
+          </td>
+          <td class="table-division">
+            <div>${user.objectives}</div>
+          </td>
+          <td class="table-division">
+            <div>${user.total_revenue}€</div>
+          </td>
+          <td class="table-division">
+            <div>${user.areas}</div>
+          </td>
+          <td class="table-division">
+            <div>${user.construction_works}</div>
+          </td>
+          <td class="table-division">
+            <div><a href="/${user._id}" class="fas fa-cart-arrow-down"></a></div>
+          </td>
+          </tr>`;
+        });
       })
       .catch(err => console.log(err));
   };
