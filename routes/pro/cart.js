8b66719f6e734eee30642c21e85620b2a3898035
Amine LAHRICHI <amine.lahrichi@dauphine.eu>
2019-11-22 12:22:44 +0100

    stripe payment try

--- routes/pro/cart.js
+++ routes/pro/cart.js
@@ -1,10 +1,41 @@
 const express = require("express");
 const router = new express.Router();
 const proModel = require("./../../models/Pro");
+
+//Stripe dependencies
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripe = require("stripe")(`${stripeSecretKey}`);
+
+//Charge
+router.post("/charge", (req, res) => {
+  let amount = 500;
+
+  stripe.customers
+    .create({
+      email: req.session.currentUser.email,
+      card: req.body.id
+    })
+    .then(customer =>
+      stripe.charges.create({
+        amount,
+        description: "Sample Charge",
+        currency: "usd",
+        customer: customer.id
+      })
+    )
+    .then(charge => res.send(charge))
+    .catch(err => {
+      console.log("Error:", err);
+      res.status(500).send({ error: "Purchase Failed" });
+    });
+});
+
+//Router pro
 router.get("/pro/cart", (req, res) => {
   res.render("pro/cart", { css: ["cart"] });
 });
 
+//Payment goes here!
 router.get("/pro/checkout/:id", (req, res) => {
   proModel
     .findOneAndUpdate(
@@ -15,6 +46,11 @@ router.get("/pro/checkout/:id", (req, res) => {
       { new: true }
     )
     .then(dbRes => {
+      // (async () => {
+      //   const paymentIntent = await stripe.paymentIntents.create({
+      //     amount: 1099,
+      //     currency: 'eur',
+      //   })
       req.session.currentUser = JSON.parse(JSON.stringify(dbRes));
       req.session.currentCart = [];
       res.redirect("/pro/dashboard");
