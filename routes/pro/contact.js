47faedd49dfd605764aa9c3bc482fafddc926b42
Amine LAHRICHI <amine.lahrichi@dauphine.eu>
2019-11-22 14:46:21 +0100

    contact done

--- routes/pro/contact.js
+++ routes/pro/contact.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/pro/contact", (req, res) => {
-  res.render("pro/contact");
+  res.render("pro/contact", { css: ["contact"] });
 });
 
 module.exports = router;
